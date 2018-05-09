fae68736b8fa3aa4ab2c0ad35295279b5a4a958d
vagrant <vagrant@homestead>
2018-05-09 02:14:42 +0000

    Create categories

--- database/migrations/2018_05_09_095118_create_categories_table.php
+++ database/migrations/2018_05_09_095118_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
+	    $table->string('name')->index()->comment('名称');
+	    $table->string('description')->nullable()->comment('描述');
+	    $table->integer('post_count')->default(0)->comment('贴子数');
             $table->timestamps();
         });
     }
