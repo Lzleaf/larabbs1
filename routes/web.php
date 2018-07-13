0e936c8114bfdcc6e66b86ba7c5152efcd54e0a9
vagrant <vagrant@homestead>
2018-07-13 09:35:05 +0000

    User can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::resource('users','UsersController',['only' => ['show','update','edit']]);
 Route::resource('categories','CategoriesController',['only' => ['show']]);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
 Route::get('/test',function(){
     echo md5('123456789kboxing01');
