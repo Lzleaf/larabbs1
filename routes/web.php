6944fb69ac824fcf3a50d1af5d7d9c1b15dbe433
vagrant <vagrant@homestead>
2018-07-16 09:05:50 +0000

    Auto translate slug

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ Auth::routes();
 Route::resource('users','UsersController',['only' => ['show','update','edit']]);
 Route::resource('categories','CategoriesController',['only' => ['show']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
+
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
 Route::get('/test',function(){
